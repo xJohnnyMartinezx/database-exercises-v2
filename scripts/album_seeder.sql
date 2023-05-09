80322fb91ee86b7a760affb1a23677288b4842d1
Johnny <mrtnz.jn@gmail.com>
2023-05-09 14:16:18 -0500

    seeded albums table

---
+++ scripts/album_seeder.sql
@@ -0,0 +1,29 @@
+USE codeup_test_db;
+
+
+INSERT INTO albums(artists_name, album_name, release_date, sales_in_millions, genre)
+VALUES
+('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop, post-disco, funk, rock'),
+('AC/DC', 'Back in Black', 1980, 29.5, 'Hard rock'),
+('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 21.7, 'Hard rock, glam rock, progressive rock'),
+('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'Country rock, soft rock, folk rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.4, 'Progressive rock'),
+('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock'),
+('Bee Gees', 'Saturday Night Fever', 1977, 21.6, 'Disco'),
+('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
+('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, pop'),
+('Grease: The Original Soundtrack', 'Grease: The Original Soundtrack from the Motion Picture', 1978, 14.4, 'Rock and roll'),
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 29.0, 'Hard rock, heavy metal, folk rock'),
+('Michael Jackson', 'Bad', 1987, 29.0, 'Pop, rhythm and blues, funk and rock'),
+('Alanis Morissette', 'Jagged Little Pill', 1995, 24.3, 'Alternative rock'),
+('Michael Jackson', 'Dangerous', 1991, 17.0, 'New jack swing, R&B and pop'),
+('Celine Dion', 'Falling into You', 1996, 21.1, 'Pop, soft rock'),
+('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.2, 'Rock'),
+('Various artists', 'Dirty Dancing', 1987, 17.9, 'Pop, rock, R&B'),
+('Adele', '21', 2011, 26.4, 'Pop, soul'),
+('Celine Dion', 'Let''s Talk About Love', 1997, 19.3, 'Pop, soft rock'),
+('Metallica', 'Metallica', 1991, 25.2, 'Heavy metal'),
+('The Beatles', '1', 2000, 23.5, 'Rock'),
+('The Beatles', 'Abbey Road', 1969, 14.4, 'Rock'),
+('Nirvana', 'Nevermind', 1991, 16.7, 'Grunge, alternative rock');
--- scripts/albums_migration_v2.sql
+++ scripts/albums_migration_v2.sql
@@ -5,11 +5,10 @@ DROP TABLE IF EXISTS albums;
 
     CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artists_firstName VARCHAR(50) NOT NULL,
-    artists_lastName VARCHAR(50) NOT NULL,
+    artists_name VARCHAR(50) NOT NULL,
     album_name VARCHAR(255) NOT NULL,
     release_date INT NOT NULL,
-    sales DOUBLE NOT NULL,
+    sales_in_millions DOUBLE NOT NULL,
     genre VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
